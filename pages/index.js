d90ecf85a0474b8959035c19eead22533d75fffe
Caio Sousa <43479716+caiosousanl@users.noreply.github.com>
2023-12-29 21:07:19 +0000

    teste ci

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 const Home = () => {
-    return <h1>Te amo meu docin, se você me ama da uma risadinha</h1>
+    return <h1>Te amo meu docin, se você me ama da uma risadinha!</h1>
 }
 
 export default Home;
